f85b2eee218a91007f6a492ab16cc216c4f18d22
Piotr Ludynia <pietrekpl18@gmail.com>
2022-09-25 12:45:56 +0200

    add passwordEncoder

--- src/main/java/com/example/springSecurity/security/ApplicationSecurityConfiguration.java
+++ src/main/java/com/example/springSecurity/security/ApplicationSecurityConfiguration.java
@@ -1,25 +1,47 @@
 package com.example.springSecurity.security;
 
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import java.net.http.HttpRequest;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class ApplicationSecurityConfiguration {
 
+    private final PasswordEncoder passwordEncoder;
+
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-        http.authorizeRequests((auth)-> auth
-                        .anyRequest().authenticated()
-                ).httpBasic(Customizer.withDefaults());
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests((auth) -> auth
+                .antMatchers("/", "index", "/css/*")
+                .permitAll()
+                .anyRequest().authenticated()
+        ).httpBasic(Customizer.withDefaults());
         return http.build();
     }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User.builder()
+                .username("user")
+                .password(passwordEncoder.encode("password"))
+                .roles("STUDENT")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
 
 }
