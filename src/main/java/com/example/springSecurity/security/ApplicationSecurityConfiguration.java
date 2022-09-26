30a2ce05cf146a79ffc809a0d0de39cf44d49b02
Piotr Ludynia <pietrekpl18@gmail.com>
2022-09-26 20:03:03 +0200

    fix 403

--- src/main/java/com/example/springSecurity/security/ApplicationSecurityConfiguration.java
+++ src/main/java/com/example/springSecurity/security/ApplicationSecurityConfiguration.java
@@ -4,6 +4,7 @@ package com.example.springSecurity.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -15,6 +16,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 import java.net.http.HttpRequest;
 
+import static com.example.springSecurity.security.ApplicationUserPermission.COURSE_WRITE;
 import static com.example.springSecurity.security.ApplicationUserRole.*;
 
 @Configuration
@@ -31,6 +33,10 @@ public class ApplicationSecurityConfiguration {
                 .authorizeRequests((auth) -> auth
                         .antMatchers("/", "index", "/css/*").permitAll()
                         .antMatchers("/students/**").hasRole(STUDENT.name())
+                        .antMatchers(HttpMethod.DELETE, "/management/students/**").hasAuthority(COURSE_WRITE.getPermision())
+                        .antMatchers(HttpMethod.PUT, "/management/students/**").hasAuthority(COURSE_WRITE.getPermision())
+                        .antMatchers(HttpMethod.POST, "/management/students/**").hasAuthority(COURSE_WRITE.getPermision())
+                        .antMatchers(HttpMethod.GET, "/management/students/**").hasAnyRole(ADMIN.name(), ADMIN_TRAINEE.name())
                         .anyRequest().authenticated()
                 ).httpBasic(Customizer.withDefaults());
         return http.build();
@@ -41,21 +47,21 @@ public class ApplicationSecurityConfiguration {
         UserDetails paulStudent = User.builder()
                 .username("paul")
                 .password(passwordEncoder.encode("password"))
-                .roles(STUDENT.name())
+                .authorities(STUDENT.getGrantedAuthorities())
                 .build();
 
 
         UserDetails peterAdmin = User.builder()
                 .username("peter")
                 .password(passwordEncoder.encode("password1234"))
-                .roles(ADMIN.name())
+                .authorities(ADMIN.getGrantedAuthorities())
                 .build();
 
 
         UserDetails adamAdmin = User.builder()
                 .username("adam")
                 .password(passwordEncoder.encode("password1234"))
-                .roles(ADMIN_TRAINEE.name())
+                .authorities(ADMIN_TRAINEE.getGrantedAuthorities())
                 .build();
 
         return new InMemoryUserDetailsManager(paulStudent, peterAdmin, adamAdmin);
--- src/main/java/com/example/springSecurity/security/ApplicationUserRole.java
+++ src/main/java/com/example/springSecurity/security/ApplicationUserRole.java
@@ -1,13 +1,17 @@
 package com.example.springSecurity.security;
 
 import com.google.common.collect.Sets;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import static com.example.springSecurity.security.ApplicationUserPermission.*;
 
 @RequiredArgsConstructor
+@Getter
 public enum ApplicationUserRole {
 
     STUDENT(Sets.newHashSet()),
@@ -19,4 +23,13 @@ public enum ApplicationUserRole {
 
 
     private final Set<ApplicationUserPermission> permissions;
+
+
+    public Set<SimpleGrantedAuthority> getGrantedAuthorities(){
+        Set<SimpleGrantedAuthority> permissions = getPermissions().stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.getPermision()))
+                .collect(Collectors.toSet());
+        permissions.add(new SimpleGrantedAuthority("ROLE_"+this.name()));
+        return permissions;
+    }
 }
